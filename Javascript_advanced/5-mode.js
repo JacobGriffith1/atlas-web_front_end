8c3fcc11ae5d7d5ba3f37c69338779bd376b98fd
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-11-21 14:14:16 -0600

    5 reborn

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -12,6 +12,26 @@ function main() {
     let spooky = changeMode(9, "bold", "uppercase", "pink", "green");
     let darkMode = changeMode(12, "bold", "capitalize", "black", "white");
     let screamMode = changeMode(12, "normal", "lowercase", "white", "black");
-};
+
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = "Happy now?"
+    const spookyBtn = document.createElement("button");
+    spookyBtn.innerHTML = "Spooky";
+    spookyBtn.onclick = function () {spooky();};
+    const darkBtn = document.createElement("button");
+    darkBtn.innerHTML = "Dark mode";
+    darkBtn.onclick = function () {darkMode();};
+    const screamBtn = document.createElement("button");
+    screamBtn.innerHTML = "Scream mode";
+    screamBtn.onclick = function () {screamMode();;}
+
+
+
+    document.body.appendChild(paragraph);
+    document.body.appendChild(spookyBtn);
+    document.body.appendChild(darkBtn);
+    document.body.appendChild(screamBtn);
+
+}
 
 main();
