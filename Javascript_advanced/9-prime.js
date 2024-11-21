d542f0be9b839dff1f02da056cde83ed479ff8e1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-11-21 00:00:09 -0600

    9, index

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,25 @@
+function countPrimeNumbers() {
+    let prime = true;
+    let pCount = 0;
+
+    for (let i = 2; i <= 100; i++) {
+        prime = true;
+        let sqrt = Math.round(Math.sqrt(i));
+        for (let div = 2; div <= sqrt; div++) {
+            if (i % div == 0) {
+                prime = false;
+                break;
+            }
+        }
+        if (prime){
+            pCount++;
+        }
+    }
+    return pCount;
+}
+
+let begin = performance.now();
+countPrimeNumbers();
+let end = performance.now();
+
+console.log("Execution time of printing countPrimeNumbers was " + (end - begin) + " milliseconds.");
