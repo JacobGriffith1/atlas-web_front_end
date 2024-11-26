1493c0c97777e99644b643851b4840b2ea6f30f8
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-11-26 00:35:04 -0600

    2-index, index.js

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -12,6 +12,17 @@ function setCookies() {
 function showCookies() {
     const cookies = document.cookie;
     const paragraph = document.createElement("p");
-    paragraph.innerHTML = `Cookies: ${cookies}`;
+    /** paragraph.innerHTML = `Cookies: ${cookies}`; <===== 0/1 VERSION */
+    paragraph.innerHTML = `Email: ${getCookie("email")} - Firstname: ${getCookie("firstname")}`;
     document.body.appendChild(paragraph);
+}
+
+/** getCookie */
+function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].split("=");
+        if (cookie[0] === name) return cookie[1];
+    }
+    return "";
 }
