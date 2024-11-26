492cd18ea360b411dbd7f06e8c40ee65d711e488
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-11-26 00:17:37 -0600

    1-index.html, index.js

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -3,8 +3,9 @@ function setCookies() {
     const firstName = document.querySelector("#firstname").value;
     const email = document.querySelector("#email").value;
 
-    document.cookie = `firstname=${firstName}`;
-    document.cookie = `email=${email}`;
+    document.cookie = `firstname=${firstName}; max-age=864000`;
+    document.cookie = `email=${email}; max-age=864000`;
+    /** max-age appended for task 2 */
 }
 
 /** showCookies */
